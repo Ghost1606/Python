6ca4eaf513b9d8779926302daaa5ec6976d8ebd5
Ghost1606 <101306645+Ghost1606@users.noreply.github.com>
2022-03-17 18:38:00 +0900

    Add files via upload

---
+++ 201987043/01.py
@@ -0,0 +1,5 @@
+name = input("이름을 입력하시오: ")
+age = int(input("나이를 입력하시오: "))
+
+year = 2022+(94-age)
+print(name,"씨는",str(year),"년에 100살이시네요!") 
---
+++ 201987043/07.py
@@ -0,0 +1,28 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+side = 100
+angle = 90
+
+t.fd(side)
+t.fd(side)
+t.rt(angle)
+t.fd(side)
+t.fd(side)
+t.rt(angle)
+t.fd(side)
+t.fd(side)
+t.rt(angle)
+t.fd(side)
+t.fd(side)
+t.backward(side)
+t.rt(angle)
+t.fd(side)
+t.fd(side)
+t.backward(side)
+t.rt(angle)
+t.fd(side)
+t.backward(side)
+t.backward(side)
+t.fd(side)
